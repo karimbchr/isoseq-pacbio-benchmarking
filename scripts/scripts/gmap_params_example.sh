bd41e9f288cf7e6bee0004c57737cd5ebe927b80
Karim <164534155+karimbchr@users.noreply.github.com>
2025-08-10 17:08:01 +0200

    Delete scripts/scripts directory

--- scripts/scripts/gmap_params_example.sh
+++
@@ -1,4 +0,0 @@
-# Build index
-gmap_build -D ./gmap_index -d tair10 reference_genome.fas
-# Map with trimming score example
-gmap -f samse -D ./gmap_index -d tair10 --end-trimming-score=-16 -t 8 mock_reads.fasta > mock_gmap.sam
